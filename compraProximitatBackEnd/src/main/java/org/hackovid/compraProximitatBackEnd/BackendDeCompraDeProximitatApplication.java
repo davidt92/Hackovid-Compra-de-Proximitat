f9a71c59d303e31e6d0e63784990ad7ceaacf53b
Administrator <admin@example.com>
2020-04-06 19:25:38 +0200

    Fisrt commit with Spring boot generated app

---
+++ compraProximitatBackEnd/src/main/java/org/hackovid/compraProximitatBackEnd/BackendDeCompraDeProximitatApplication.java
@@ -0,0 +1,13 @@
+package org.hackovid.compraProximitatBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeCompraDeProximitatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeCompraDeProximitatApplication.class, args);
+	}
+
+}
---
+++ compraProximitatBackEnd/src/test/java/org/hackovid/compraProximitatBackEnd/BackendDeCompraDeProximitatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hackovid.compraProximitatBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeCompraDeProximitatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
